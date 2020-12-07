cccb167d9ae81a1aa2d3ae0657d4a541a5f91d12
CD-Mackay <connor.mackay@gmail.com>
2020-12-07 19:11:40 +0000

    added HTML for /hello page

--- express_server.js
+++ express_server.js
@@ -13,7 +13,11 @@ app.get('/', (request, response) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(URLDatabase);
-})
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
