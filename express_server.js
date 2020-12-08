0af81c2eb4f7b5a6e1fef50535e2c8feb7f2865e
CD-Mackay <connor.mackay@gmail.com>
2020-12-08 01:24:21 +0000

    cleaned and linted code

--- express_server.js
+++ express_server.js
@@ -4,12 +4,14 @@ const port = 8080;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Set view enging to EJS
+//Set view engine to EJS
 app.set('view engine', 'ejs');
 
+//Generate unique ID for new URLS
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
+
 // Database of valid URLS with unique Ids
 const URLDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,9 +29,10 @@ app.post('/urls', (req, res) => {
 app.get('/urls/new', (req, res) => {
   res.render('pages/urls_new');
 });
-// /urls displays URLDatabase object 
+
+// /urls displays URLDatabase object
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: URLDatabase}
+  const templateVars = { urls: URLDatabase};
   res.render('pages/urls_index', templateVars);
 });
 
@@ -38,13 +41,14 @@ app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = URLDatabase[shortURL];
   res.redirect(301, longURL);
-  
-})
+});
+
 // Hello page contains html data
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
 // Check if server is running
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-})
+});
